76b4583b0fb09bb07ba70045b962c373438037e4
owls4all <owls4all@gmail.com>
2024-10-23 10:42:35 -0700

    solved challenge

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import turtle
-
-# YOUR CODE HERE
+t= turtle
+t.up()         #  |
+t.goto(-50,50) #  |- I wanted it centered at 0,0
+t.down()       #  |
+for i in range(4):
+    t.fd(100)
+    t.rt(90)
+t.fd(50)
+t.rt(45)
+for i in range(4):
+    t.fd(71)
+    t.rt(90)
 
 turtle.exitonclick()
